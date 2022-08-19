51b0d60ba3e7e729c13b33a67c777426a7b420b8
Chanyoung Yun <38937802+ghk71@users.noreply.github.com>
2022-08-19 12:24:04 +0900

    popup

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -71,3 +71,15 @@ function toggleClassSearch() {
     document.querySelector('.search-content input').focus();
   }, 400);
 }
+
+$(window).load(function () {
+  $(".trigger_popup_fricc").click(function(){
+     $('.hover_bkgr_fricc').show();
+  });
+  $('.hover_bkgr_fricc').click(function(){
+      $('.hover_bkgr_fricc').hide();
+  });
+  $('.popupCloseButton').click(function(){
+      $('.hover_bkgr_fricc').hide();
+  });
+});
