41f92f6714aed9c29e91ea3ee5fffe58d00a7946
Chanyoung Yun <38937802+ghk71@users.noreply.github.com>
2023-01-16 17:40:31 +0900

    55

---
+++ assets/javascripts/food_map.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    mapboxgl.accessToken = 'pk.eyJ1Ijoia2hhcmlzbWExMSIsImEiOiJjazM1M3dra2cwZjM0M2NwZXhmdWEybHIyIn0.ALDvfHZ6cPKoika-aEL65A';
+    
+    const map = new mapboxgl.Map({
+        container: 'main-map',
+        style: 'mapbox://styles/mapbox/streets-v12',
+        center: [126.6548550, 37.3839120],
+        zoom: 12.6
+    });
+})
