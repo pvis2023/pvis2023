ad94d47dc9656c49000bfcb6ff3c70d01e817170
Chanyoung Yun <38937802+ghk71@users.noreply.github.com>
2023-01-16 17:59:05 +0900

    55

--- assets/javascripts/food_map.js
+++ assets/javascripts/food_map.js
@@ -4,7 +4,43 @@ window.onload = function() {
     const map = new mapboxgl.Map({
         container: 'main-map',
         style: 'mapbox://styles/mapbox/streets-v12',
-        center: [126.6548550, 37.3839120],
-        zoom: 12.6
+        center: [126.95, 37.55],
+        zoom: 10
+    });
+
+    map.on('load', function() {
+        map.loadImage('https://pvis2023.github.io//pvis2023/assets/images/placeholder.png', function(error, image) {
+            if(error) throw error;
+            map.addImage('place-holder', image);
+        })
+    });
+
+    var geojson = {
+        type: 'geojson',
+        data: {
+            type: 'FeatureCollection',
+            features: []
+        }
+    };
+
+    geojson.data.features.push({
+        type:'Feature',
+        geometry: {
+            type:'Point',
+            coordinates: [126.9864882, 37.5692044]
+        },
+        properties: {
+            image: 'place-holder'
+        }
+    });
+
+    map.addSource('test', geojson);
+    map.addLayer({
+        id: 'test',
+        type: 'symbol',
+        source: 'test',
+        layout: {
+            'icon-image': '{image}'
+        }
     });
 }
