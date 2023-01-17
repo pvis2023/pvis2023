91de9c48394da84400a9455433be106e148a3137
Chanyoung Yun <38937802+ghk71@users.noreply.github.com>
2023-01-17 17:11:41 +0900

    Update committee.js

--- assets/javascripts/committee.js
+++ assets/javascripts/committee.js
@@ -195,7 +195,7 @@ window.onload = function() {
 
 function generalDescription() {
     var data = general[0];
-    
+
     var generalChair = `
         <img src="https://pvis2023.github.io//pvis2023/assets/images/oc/${data['first_name']}_${data['last_name']}.jpg" class="circle" width="150" height="150"><br>
     `;
@@ -241,8 +241,7 @@ function generalDescription() {
 }
 
 function paperDescription() {
-    var paperChair;
-    console.log(paper.length);
+    var paperChair=``;
     
     for(var i=0;i<paper.length;i++) {
         var data = paper[i];
