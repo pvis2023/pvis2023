105ff572fe3d4753dca7763f60b95e4234470843
Chanyoung Yun <38937802+ghk71@users.noreply.github.com>
2023-01-17 16:19:37 +0900

    Update food_map2.js

--- assets/javascripts/food_map2.js
+++ assets/javascripts/food_map2.js
@@ -252,7 +252,7 @@ window.onload = function() {
         container: 'main-map',
         style: 'mapbox://styles/mapbox/streets-v12',
         center: [126.987, 37.569],
-        zoom: 15
+        zoom: 12
     });
 
     setTimeout(makeMap, 500);
