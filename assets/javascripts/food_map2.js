5d422c43c3e0d2f9b5ab2f0bdd401b60f72928a5
Chanyoung Yun <38937802+ghk71@users.noreply.github.com>
2023-01-18 17:09:36 +0900

    51

--- assets/javascripts/food_map2.js
+++ assets/javascripts/food_map2.js
@@ -243,7 +243,30 @@ var hotel_data = [
     }
 ]
 
+var menuData = {
+    'home»': "/pvis2023/",
+    'contribute»':'/pvis2023/pages/cfp',
+    'committees»':'/pvis2023/pages/committees',
+    'venue & travel»':'/pvis2023/pages/venue',
+    'sponsor»':'/pvis2023/pages/sponsor',
+    'contact»':'/pvis2023/pages/contact'
+}
+
 window.onload = function() {
+    var menuElem = document.getElementsByClassName('menu')[0];
+
+    inHtml = ``;
+
+    for(var menu in menuData) {
+        var link = menuData[menu];
+
+        inHtml += `
+            <a href="${link}">${menu}</a>
+        `;
+    }
+
+    menuElem.innerHTML = inHtml;
+    
     mapboxgl.accessToken = 'pk.eyJ1Ijoia2hhcmlzbWExMSIsImEiOiJjazM1M3dra2cwZjM0M2NwZXhmdWEybHIyIn0.ALDvfHZ6cPKoika-aEL65A';
 
     popup = new mapboxgl.Popup();
