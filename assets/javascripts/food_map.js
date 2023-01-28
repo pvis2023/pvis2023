95a2f21974ff10d471439a1719205944dd9f2c5c
Chanyoung Yun <38937802+ghk71@users.noreply.github.com>
2023-01-29 05:37:35 +0900

    163

--- assets/javascripts/food_map.js
+++ assets/javascripts/food_map.js
@@ -487,12 +487,40 @@ var hotel_data = [
 ];
 
 var menuData = {
-    'home»': "/pvis2023/",
-    'contribute»':'/pvis2023/pages/cfp',
-    'committees»':'/pvis2023/pages/committees',
-    'travel & venue»':'/pvis2023/pages/venue',
-    'sponsor»':'/pvis2023/pages/sponsor',
-    'contact»':'/pvis2023/pages/contact'
+    'home»': {
+        'link': "/pvis2023/",
+        'sub':undefined
+    },
+    'contribute»': {
+        'link': '/pvis2023/pages/cfp',
+        'sub': {
+            'Full Paper':'/pvis2023/pages/cfps/cfp_full',
+            'Notes':'/pvis2023/pages/cfps/cfp_notes',
+            'Posters':'/pvis2023/pages/cfps/cfp_posters',
+            'Contest':'/pvis2023/pages/cfps/cfp_story',
+            'Meets':'/pvis2023/pages/cfps/cfp_meet',
+        }
+    },
+    'committees»': {
+        'link':'/pvis2023/pages/committees',
+        'sub':undefined
+    },
+    'travel & venue»': {
+        'link': '/pvis2023/pages/venue',
+        'sub': {
+            'Travel': '/pvis2023/pages/venue',
+            'Venue': '/pvis2023/pages/venue',
+            'Visa': '/pvis2023/pages/venue'
+        }
+    },
+    'sponsor»': {
+        'link': '/pvis2023/pages/sponsor',
+        'sub':undefined
+    },
+    'contact»': {
+        'link': '/pvis2023/pages/contact',
+        'sub': undefined
+    }
 }
 
 window.onload = function() {
@@ -501,12 +529,25 @@ window.onload = function() {
     inHtml = ``;
 
     for(var menu in menuData) {
-        var link = menuData[menu];
+        var sub = menuData[menu];
 
-        inHtml += `
-            <a href="${link}">${menu}</a>
-        `;
+        inHtml += `<li><a href="${sub['link']}">${menu}</a>`
+
+        if(sub['sub'] === undefined) {
+            inHtml += `</li>`;
+            continue;
+        }
+
+        inHtml += `<ul>`;
+        for(var subMenu in sub['sub']) {
+            var link = sub['sub'][subMenu];
+
+            inHtml += `<li><a href="${link}">${subMenu}</a></li>`;
+        }
+        inHtml += `</ul></li>`;
     }
+
+    console.log(inHtml);
 
     menuElem.innerHTML = inHtml;
 
