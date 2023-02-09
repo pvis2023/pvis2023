e386c28a75a0678defefd6026a4e221312a3fc3f
Chanyoung Yun <38937802+ghk71@users.noreply.github.com>
2023-02-09 15:23:07 +0900

    173173

--- assets/javascripts/committee.js
+++ assets/javascripts/committee.js
@@ -193,55 +193,6 @@ var sponsor = [
     }
 ]
 
-var menuData = {
-    'Home': {
-        'link': "/pvis2023/",
-        'sub':undefined
-    },
-    'Contribute': {
-        'link': '/pvis2023/pages/cfp',
-        'sub': {
-            'Full Paper':'/pvis2023/pages/cfps/cfp_full',
-            'Visualization Notes':'/pvis2023/pages/cfps/cfp_notes',
-            'Posters':'/pvis2023/pages/cfps/cfp_posters',
-            'Storytelling Contest':'/pvis2023/pages/cfps/cfp_story',
-            'Workshop':'/pvis2023/pages/cfps/cfp_meet',
-        }
-    },
-    'Program': {
-        'link': '/pvis2023/pages/program',
-        'sub': {
-            'Keynotes': '/pvis2023/pages/programs/keynotes',
-            'Sessions': '/pvis2023/pages/programs/sessions',
-            'Awards': '/pvis2023/pages/programs/awards'
-        }
-    },
-    'Registration': {
-        'link': '/pvis2023/pages/registration',
-        'sub': undefined
-    },
-    'Committees': {
-        'link':'/pvis2023/pages/committees',
-        'sub':undefined
-    },
-    'Travel & Venue': {
-        'link': '/pvis2023/pages/venue',
-        'sub': {
-            'Travel Information': '/pvis2023/pages/venue#travel',
-            'Venue': '/pvis2023/pages/venue#venue',
-            'Visa': '/pvis2023/pages/venue#visa'
-        }
-    },
-    'Sponsor': {
-        'link': '/pvis2023/pages/sponsor',
-        'sub':undefined
-    },
-    'Contact': {
-        'link': '/pvis2023/pages/contact',
-        'sub': undefined
-    }
-}
-
 window.onload = function() {
     createMenu();
 
