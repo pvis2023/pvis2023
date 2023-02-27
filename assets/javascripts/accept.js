1a6fbfcd3d3a290ea53fab04ce6e73da37b78c2a
Chanyoung Yun <38937802+ghk71@users.noreply.github.com>
2023-02-28 04:03:28 +0900

    136

--- assets/javascripts/accept.js
+++ assets/javascripts/accept.js
@@ -3,112 +3,291 @@ var paper_data = {
         {
             'id': "7946",
             'title': "FraudAuditor: A Visual Analytics Approach for Collusive Fraud in Health Insurance",
-            'author': "Jiehui Zhou"
+            'author': 
+            [
+                {'name': "Jiehui Zhou", 'institution': "Zhejiang University"},
+                {'name': "Xumeng Wang", 'institution': "Nankai University"},
+                {'name': "Jie Wang", 'institution': "Zhejiang University"},
+                {'name': "Hui Ye", 'institution': "Zhejiang University"},
+                {'name': "Huanliang Wang", 'institution': "Zhejiang University"},
+                {'name': "Zihan Zhou", 'institution': "Zhejiang University"},
+                {'name': "Dongming Han", 'institution': "Zhejiang University"},
+                {'name': "Haochao Ying", 'institution': "Zhejiang University"},
+                {'name': "Jian Wu", 'institution': "Zhejiang University"},
+                {'name': "Wei Chen", 'institution': "Zhejiang University"}
+            ]
+        },
+        {
+            'id': "7905",
+            'title': "An Empirical Guide for Visualization Consistency in Multiple Coordinated Views",
+            'author': 
+            [
+                {'name': "Shaocong Tan", 'institution': "Peking University"},
+                {'name': "Chufan Lai", 'institution': "Peking University"},
+                {'name': "Xiaolong (Luke) Zhang", 'institution': "Penn State University"},
+                {'name': "Xiaoru Yuan", 'institution': "Peking University"}
+            ]
         },
         {
             'id': "8295",
             'title': "Parallel Assemblies Plot, a visualization tool to explore categorical and quantitative data: application to digital mobility outcomes",
-            'author': "Alma Cantu"
+            'author': 
+            [
+                {'name': "Alma Cantu", 'institution': "Newcastle University"},
+                {'name': "Encarna Mico Amigo", 'institution': "Newcastle University"},
+                {'name': "Silvia Del Din", 'institution': "Newcastle University"},
+                {'name': "Sara Johansson", 'institution': "Newcastle University"},
+            ]
         },
         {
             'id': "4476",
             'title': "How Can We Improve Data Quality for Machine Learning? A Visual Analytics System using Data and Process-driven Strategies",
-            'author': "Hyein Hong"
+            'author': 
+            [
+                {'name': "Hyein Hong", 'institution': "Sejong University"},
+                {'name': "Sangbong Yoo", 'institution': "Sejong University"},
+                {'name': "Yejin Jin", 'institution': "Sejong University"},
+                {'name': "Yun Jang", 'institution': "Sejong University"},
+            ]
         },
         {
             'id': "3258",
             'title': "GraphDescriptor: Augmenting Node-Link Diagrams With Textual Descriptions",
-            'author': "Jiacheng Pan"
+            'author': 
+            [
+                {'name': "Jiacheng Pan", 'institution': "Zhejiang University"},
+                {'name': "Zihan Yan", 'institution': "MIT Media Lab"},
+                {'name': "Zihan Zhou", 'institution': "Zhejiang University"},
+                {'name': "Xiaodong Zhao", 'institution': "Zhejiang University"},
+                {'name': "Shenghui Cheng", 'institution': "Westlake University"},
+                {'name': "Dongming Han", 'institution': "Zhejiang University"},
+                {'name': "Jian Chen", 'institution': "he Ohio State University"},
+                {'name': "Mingliang Xu", 'institution': "Zhengzhou University"},
+                {'name': "Wei Chen", 'institution': "Zhejiang University"}
+            ]
         },
         {
             'id': "5693",
             'title': "SDRQuerier: A Visual Querying Framework for Cross-National Survey Data Recycling",
-            'author': "Yamei Tu"
+            'author': 
+            [
+                {'name': "Yamei Tu", 'institution': "The Ohio State University"},
+                {'name': "Olga Li", 'institution': "Polish Academy of Sciences"},
+                {'name': "Junpeng Wang", 'institution': "Palo Alto"},
+                {'name': "Han-Wei Shen", 'institution': "The Ohio State University"},
+                {'name': "Przemek Powalko", 'institution': "Polish Academy of Sciences"},
+                {'name': "Irina Tomescu-Dubrow", 'institution': "Polish Academy of Sciences"},
+                {'name': "Kazimierz M. Slomczynski", 'institution': "Polish Academy of Sciences"},
+                {'name': "Spyros Blanas", 'institution': "The Ohio State University"},
+                {'name': "J. Craig Jenkins", 'institution': "The Ohio State University"},
+            ]
         },
         {
             'id': "8541",
             'title': "Investigating Animal Infectious Diseases with Visual Analytics",
-            'author': "Yun-Hsin Kuo"
+            'author': 
+            [
+                {'name': "Yun-Hsin Kuo", 'institution': "University of California"},
+                {'name': "Beatriz Martínez-López", 'institution': "University of California"},
+                {'name': "Kwan-Liu Ma", 'institution': "University of California at Davis"}
+            ]
         },
         {
             'id': "7465",
             'title': "MySemCloud: Semantic-aware Word Cloud Editing",
-            'author': "MSc Anaïs Villedieu"
+            'author': 
+            [
+                {'name': "Michael Huber", 'institution': "TU Wien"},
+                {'name': "Martin Nöllenburg", 'institution': "TU Wien"},
+                {'name': "MSc Anaïs Villedieu", 'institution': "TU Wien"},
+            ]
         },
         {
             'id': "4333",
             'title': "LinSets.zip: Compressing Linear Set Diagrams",
-            'author': "Markus Wallinger"
+            'author': 
+            [
+                {'name': "Markus Wallinger", 'institution': "TU Wien"},
+                {'name': "Alexander Dobler", 'institution': "TU Wien"},
+                {'name': "Martin Nöllenburg", 'institution': "TU Wien"},
+            ]
         },
         {
             'id': "1448",
             'title': "NFTVis: Visual Analysis of NFT Performance",
-            'author': "Fan Yan"
+            'author': 
+            [
+                {'name': "Fan Yan,", 'institution': "Zhejiang University"},
+                {'name': "Xumeng Wang", 'institution': "Nankai University"},
+                {'name': "Ketian Mao", 'institution': "Zhejiang University"},
+                {'name': "Wei Zhang", 'institution': "Zhejiang University"},
+                {'name': "Wei Chen", 'institution': "Zhejiang University"}
+            ]
         },
         {
             'id': "2112",
             'title': "Feature Learning for Nonlinear Dimensionality Reduction toward Maximal Extraction of Hidden Patterns",
-            'author': "Takanori Fujiwara"
+            'author': 
+            [
+                {'name': "Takanori Fujiwara", 'institution': "Linköping University"},
+                {'name': "Yun-Hsin Kuo", 'institution': "University of California"},
+                {'name': "Kwan-Liu Ma", 'institution': "University of California at Davis"},
+            ]
         },
         {
             'id': "2994",
             'title': "Visualizing Interaction Networks and Evidence in Biomedical Corpora",
-            'author': "Enrique Noriega-Atala"
+            'author': 
+            [
+                {'name': "Enrique Noriega-Atala", 'institution': "The University of Arizona"},
+                {'name': "Md Rahat-uz Zaman", 'institution': "The University of Arizona"},
+                {'name': "Stephen Kobourov", 'institution': "The University of Arizona"},
+                {'name': "Ruchika Bhat", 'institution': "The University of Arizona"},
+                {'name': "Mladen Jergovic", 'institution': "The University of Arizona"},
+                {'name': "Janko Nikolich-Sugich", 'institution': "The University of Arizona"}
+            ]
         },
         {
             'id': "3219",
             'title': "A Study of Zooming, Interactive Lenses and Overview+Detail Techniques in Collaborative Map-based Tasks",
-            'author': "Lingyun Yu"
+            'author': 
+            [
+                {'name': "Yu Liu", 'institution': "Xi'an Jiaotong-Liverpool University"},
+                {'name': "Zhichao Zhang", 'institution': "Xi'an Jiaotong-Liverpool University"},
+                {'name': "Yushan Pan", 'institution': "Xi'an Jiaotong-Liverpool University"},
+                {'name': "Yue Li", 'institution': "Xi'an Jiaotong-Liverpool University"},
+                {'name': "Hai-Ning Liang", 'institution': "Xi'an Jiaotong-Liverpool University"},
+                {'name': "Paul Craig", 'institution': "Xi'an Jiaotong-Liverpool University"},
+                {'name': "Lingyun Yu", 'institution': "Xi'an Jiaotong-Liverpool University"}
+            ]
         },
         {
             'id': "4089",
             'title': "Edit-History Vis: An Interactive Visual Exploration and Analysis on Wikipedia Edit History",
-            'author': "Yuhan Guo"
+            'author': 
+            [
+                {'name': "Yuhan Guo", 'institution': "Peking University"},
+                {'name': "Qin Han", 'institution': "Peking University"},
+                {'name': "Yuke Lou", 'institution': "Peking University"},
+                {'name': "Yiming Wang", 'institution': "Peking University"},
+                {'name': "Can Liu", 'institution': "Peking University"},
+                {'name': "Xiaoru Yuan", 'institution': "Peking University"}
+            ]
         },
         {
             'id': "5370",
             'title': "Studies of Part-to-Whole Glanceable Visualizations on Smartwatch Faces",
-            'author': "Tanja Blascheck"
+            'author': 
+            [
+                {'name': "Tanja Blascheck", 'institution': "University of Stuttgart"},
+                {'name': "Lonni Besançon", 'institution': "Linköpings universitet"},
+                {'name': "Anastasia Bezerianos", 'institution': "Université Paris-Saclay"},
+                {'name': "Bongshin Lee", 'institution': "Microsoft Research"},
+                {'name': "Alaul Islam", 'institution': "Université Paris-Saclay"},
+                {'name': "Tingying He", 'institution': "University Paris Saclay"},
+                {'name': "Petra Isenberg", 'institution': "University Paris Saclay"}
+            ]
         },
         {
             'id': "5584",
             'title': "Interactive Transformations and Visual Assessment of Noisy Event Sequences: An Application in En-Route Air Traffic Control",
-            'author': "Peilin Yu"
+            'author': 
+            [
+                {'name': "Peilin Yu", 'institution': "Linköping University"},
+                {'name': "Aida Nordman", 'institution': "Linköping University"},
+                {'name': "Lothar Meyer", 'institution': "Swedish Air Navigation Service"},
+                {'name': "Supathida Boonsong", 'institution': "Swedish Air Navigation Service"},
+                {'name': "Katerina Vrotsou", 'institution': "Linköping University"},
+            ]
         },
         {
             'id': "5635",
             'title': "Neural Stream Functions",
-            'author': "Skylar Wolfgang Wurster"
+            'author': 
+            [
+                {'name': "Skylar Wolfgang Wurster", 'institution': "The Ohio State University"},
+                {'name': "Hanqi Guo", 'institution': "The Ohio State University"},
+                {'name': "Tom Peterka", 'institution': " Argonne National Laboratory"},
+                {'name': "Han-Wei Shen", 'institution': "The Ohio State University"},
+            ]
         },
         {
             'id': "7856",
             'title': "How Does Attention Work in Vision Transformers? A Visual Analytics Attempt",
-            'author': "Yiran Li"
+            'author': 
+            [
+                {'name': "Yiran Li", 'institution': "University of California"},
+                {'name': "Junpeng Wang", 'institution': "Palo Alto"},
+                {'name': "Xin Dai", 'institution': "Palo Alto"},
+                {'name': "Liang Wang", 'institution': "Palo Alto"},
+                {'name': "Chin-Chia Michael Yeh", 'institution': "Palo Alto"},
+                {'name': "Yan Zheng", 'institution': "Palo Alto"},
+                {'name': "Wei Zhang", 'institution': "Palo Alto"},
+                {'name': "Kwan-Liu Ma", 'institution': "University of California at Davis"}
+            ]
         },
         {
             'id': "8044",
             'title': "Understanding 3D Data Videos: From Screens to Virtual Reality",
-            'author': "Leni Yang"
+            'author': 
+            [
+                {'name': "Leni Yang", 'institution': "The Hong Kong University of Science and Technology"},
+                {'name': "Aoyu Wu", 'institution': "The Hong Kong University of Science and Technology"},
+                {'name': "Wai Tong", 'institution': "The Hong Kong University of Science and Technology"},
+                {'name': "Xian Xu", 'institution': "The Hong Kong University of Science and Technology"},
+                {'name': "Zheng Wei", 'institution': "The Hong Kong University of Science and Technology"},
+                {'name': "Huamin Qu", 'institution': "The Hong Kong University of Science and Technology"}
+            ]
         },
         {
             'id': "8193",
             'title': "LabelVizier: Interactive Validation and Relabeling for Technical Text Annotations",
-            'author': "Xiaoyu Zhang"
+            'author': 
+            [
+                {'name': "Xiaoyu Zhang", 'institution': "University of California"},
+                {'name': "Xiwei Xuan", 'institution': "University of California"},
+                {'name': "Alden Dima", 'institution': "National Institute of Standards and Technology"},
+                {'name': "Thurston Sexton", 'institution': "National Institute of Standards and Technology"},
+                {'name': "Kwan-Liu Ma", 'institution': "University of California at Davis"}
+            ]
         },
         {
             'id': "8293",
             'title': "Efficient Raycasting of Volumetric Depth Images for Remote Visualization of Large Volumes at High Frame Rates",
-            'author': "Aryaman Gupta"
+            'author': 
+            [
+                {'name': "Aryaman Gupta", 'institution': "Technische Universität Dresden"},
+                {'name': "Ulrik Günther", 'institution': "Max Planck Institute of Molecular Cell Biology and Genetics"},
+                {'name': "Pietro Incardona", 'institution': "Max Planck Institute"},
+                {'name': "Guido Reina", 'institution': "University of Stuttgart"},
+                {'name': "Steffen Frey", 'institution': "University of Groningen"},
+                {'name': "Stefan Gumhold", 'institution': "TU Dresden"},
+                {'name': "Ivo F. Sbalzarini", 'institution': "TU Dresden"}
+            ]
         },
         {
             'id': "8886",
             'title': "Federated Visualization: A Privacy-preserving Strategy for Aggregated Visual Query",
-            'author': "Yating Wei"
+            'author': 
+            [
+                {'name': "Wei Chen", 'institution': "Zhejiang University"},
+                {'name': "Yating Wei", 'institution': "Zhejiang University"},
+                {'name': "Zhiyong Wang", 'institution': "Zhejiang University"},
+                {'name': "Shuyue Zhou", 'institution': "Zhejiang University"},
+                {'name': "Bingru Lin", 'institution': "Zhejiang University"},
+                {'name': "Zhiguang Zhou", 'institution': "Zhejiang University of Finance and Economics"}
+            ]
         },
         {
             'id': "9366",
             'title': "Dimensionality Explorer for Single-Cell Analysis",
-            'author': "Haejin Jeong"
+            'author': 
+            [
+                {'name': "Haejin Jeong", 'institution': "Korea University"},
+                {'name': "Hyoung-Oh Jeon", 'institution': "Ulsan National Institute of Science and Technology (UNIST)"},
+                {'name': "Semin Lee", 'institution': "Ulsan National Institute of Science and Technology (UNIST)"},
+                {'name': "Won-Ki Jeong", 'institution': "Korea University"},
+            ]
         }
     ],
     'notes': [
@@ -202,32 +381,20 @@ window.addEventListener('load', function() {
 
         var content = paper_data[id];
 
-        if (id == 'full') {
-            for(var paper_content of content) {
+        for(var paper_content of content) {
+            inHtml += `
+                <paper>
+                    <span class='title'>(${paper_content['id']}) ${paper_content['title']}</span>
+            `;
+            
+            for(var author of paper_content['author']) {
                 inHtml += `
-                    <paper>
-                        <span class='title'>(${paper_content['id']}) ${paper_content['title']}</span>
-                        <p>Main Author: ${paper_content['author']}</p>
-                    </paper>
+                    <ul>
+                        <li class='author'>${author['name']} (${author['institution']})</li>
+                    </ul>
                 `;
             }
-        }
-        else {
-            for(var paper_content of content) {
-                inHtml += `
-                    <paper>
-                        <span class='title'>(${paper_content['id']}) ${paper_content['title']}</span>
-                `;
-                
-                for(var author of paper_content['author']) {
-                    inHtml += `
-                        <ul>
-                            <li class='author'>${author['name']} (${author['institution']})</li>
-                        </ul>
-                    `;
-                }
-                inHtml += `</paper>`;                
-            }
+            inHtml += `</paper>`;                
         }
 
         obj.innerHTML = inHtml;
