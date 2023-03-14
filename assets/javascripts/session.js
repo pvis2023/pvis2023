40e5e0edaaa2d7f31d09c89b4d34ab9bc746ac5c
Chanyoung Yun <38937802+ghk71@users.noreply.github.com>
2023-03-14 16:54:38 +0900

    Update session.js

--- assets/javascripts/session.js
+++ assets/javascripts/session.js
@@ -2,6 +2,13 @@ window.addEventListener('load', function() {
     var objs = document.getElementsByTagName('paper');
     
     for(var obj of objs) {
-        console.log(obj);
+        var inHtml = ``;
+
+        var types = obj.classList[1];
+        var id = obj.classList[2];
+        var tag = obj.classList[3];
+
+        var data = paper_data[types].find(e => e.id == id);
+        console.log(data);
     }
 });
