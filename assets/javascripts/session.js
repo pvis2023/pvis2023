d3a3181c4dcb33c23d3a15fc07278ce1f17397eb
Chanyoung Yun <38937802+ghk71@users.noreply.github.com>
2023-04-10 19:51:15 +0900

    315

--- assets/javascripts/session.js
+++ assets/javascripts/session.js
@@ -23,22 +23,22 @@ window.addEventListener('load', function() {
         else {
             if(award == 'full_best') {
                 inHtml += `
-                    <span class='title'>[Best Paper][${tag}] ${paper_content['title']}&nbsp;&nbsp;<span class="fa fa-star awarded"></span><span class="fa fa-star awarded"></span><span class="fa fa-star awarded"></span></span>
+                    <span class='title' style="color:#FFD700;">[Best Paper][${tag}] ${paper_content['title']}&nbsp;&nbsp;<span class="fa fa-star awarded"></span><span class="fa fa-star awarded"></span><span class="fa fa-star awarded"></span></span>
                 `;
             }
             else if(award == 'full_honor') {
                 inHtml += `
-                    <span class='title'>[Best Paper Honorable Mention][${tag}] ${paper_content['title']}&nbsp;&nbsp;<span class="fa fa-star awarded"></span><span class="fa fa-star awarded"></span></span>
+                    <span class='title' style="color:#FFD700;">[Paper Honorable Mention][${tag}] ${paper_content['title']}&nbsp;&nbsp;<span class="fa fa-star awarded"></span><span class="fa fa-star awarded"></span></span>
                 `;
             }
             else if(award == 'note_best') {
                 inHtml += `
-                    <span class='title'>[Best Paper Honorable Mention][${tag}] ${paper_content['title']}&nbsp;&nbsp;<span class="fa fa-star awarded" style="color:green;"></span><span class="fa fa-star awarded" style="color:green;"></span><span class="fa fa-star awarded" style="color:green;"></span></span>
+                    <span class='title' style="color:green;">[Best Visualization Notes][${tag}] ${paper_content['title']}&nbsp;&nbsp;<span class="fa fa-star awarded" style="color:green;"></span><span class="fa fa-star awarded" style="color:green;"></span><span class="fa fa-star awarded" style="color:green;"></span></span>
                 `;
             }
             else if(award == 'note_honor') {
                 inHtml += `
-                    <span class='title'>[Best Paper Honorable Mention][${tag}] ${paper_content['title']}&nbsp;&nbsp;<span class="fa fa-star awarded" style="color:green;"></span><span class="fa fa-star awarded" style="color:green;"></span></span>
+                    <span class='title' style="color:green;">[Visualization Notes Honorable Mention][${tag}] ${paper_content['title']}&nbsp;&nbsp;<span class="fa fa-star awarded" style="color:green;"></span><span class="fa fa-star awarded" style="color:green;"></span></span>
                 `;
             }
         }
